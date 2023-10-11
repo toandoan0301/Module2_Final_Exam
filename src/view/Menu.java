40e48cb3f5b35ed9fea118dea9f946d82806be5b
toandoan0301 <toan.doanchi044@gmail.com>
2023-10-11 11:11:17 +0700

    up lân 2

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -160,30 +160,24 @@ public class Menu {
             choice = Validate.inputChoice();
             List<Product> list= productManager.findAll();
             if (choice == 1) {
-                list.sort(new Comparator<Product>() {
-                    @Override
-                    public int compare(Product o1, Product o2) {
-                        if(o1.getPrice()>o2.getPrice()){
-                            return 1;
-                        }else if(o1.getPrice()<o2.getPrice()){
-                            return -1;
-                        }
-                        return 0;
+                list.sort((o1, o2) -> {
+                    if(o1.getPrice()>o2.getPrice()){
+                        return 1;
+                    }else if(o1.getPrice()<o2.getPrice()){
+                        return -1;
                     }
+                    return 0;
                 });
                 showListProduct(list);
                 break;
             } else if (choice == 2) {
-                list.sort(new Comparator<Product>() {
-                    @Override
-                    public int compare(Product o1, Product o2) {
-                        if(o1.getPrice()>o2.getPrice()){
-                            return -1;
-                        }else if(o1.getPrice()<o2.getPrice()){
-                            return 1;
-                        }
-                        return 0;
+                list.sort((o1, o2) -> {
+                    if(o1.getPrice()>o2.getPrice()){
+                        return -1;
+                    }else if(o1.getPrice()<o2.getPrice()){
+                        return 1;
                     }
+                    return 0;
                 });
                 showListProduct(list);
                 break;
